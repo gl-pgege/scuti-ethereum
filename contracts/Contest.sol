5af7ddc878df1404f3adf4a9e488aa3527454edd
gl-clee <clee@goodlabs.studio>
2021-07-09 13:08:44 -0500

    Updated Smart Contract

--- contracts/Contest.sol
+++ contracts/Contest.sol
@@ -1,23 +1,16 @@
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity 0.8.6;
 
 contract Contest {
     address public owner;
 
- 
-
     uint public endTime;
-    uint public currentTime = block.timestamp;
     
     uint public leaderScore;
     address public leaderAddress;
     
     // for contract to be started you need to have contract amount > 0
-    bool public contractStarted;
+    bool public contractFunded;
     uint public contractAmount;
-    
-    uint public minimumContractAmount = 1000;
-
- 
 
     // Modifiers
     modifier onlyOwner(){
@@ -25,21 +18,35 @@ contract Contest {
         _;
     }
 
- 
-
-    modifier startContract(){
-        require(contractStarted && owner != msg.sender && contractAmount >= minimumContractAmount);
+    modifier contractIsFunded(){
+        require(contractFunded);
         _;
     }
-
- 
-
-    modifier contractEnded(){
-        require(!contractStarted && leaderAddress == msg.sender && contractAmount >= minimumContractAmount);
+    
+    modifier pastEndTime(){
+        require(block.timestamp > endTime);
         _;
     }
-
- 
+    
+    modifier beforeEndTime(){
+        require(block.timestamp < endTime);
+        _;
+    }
+    
+    modifier onlyLeader(){
+        require(leaderAddress == msg.sender);
+        _;
+    }
+    
+    modifier notOwner(){
+        require(owner != msg.sender);
+        _;
+    }
+    
+    modifier ownerWithdrawalValidation(){
+        require(leaderAddress == address(0) || (leaderAddress == address(0) && block.timestamp > endTime));
+        _;
+    }
 
     // constructor that ensures the contract owner funds the contract on deployment
     constructor(address _owner, uint _contractAmount) public{
@@ -47,64 +54,45 @@ contract Contest {
         contractAmount = _contractAmount;
         leaderScore = 100;
     }
-
- 
-
-    // function to fund the contract
-    function fundContract() public payable{
-        require(contractAmount == msg.value);
-        contractStarted = true;
-    }
-
- 
-
-    function getLeaderScore() public view returns (uint) {
-        return leaderScore;
+    
+    function currentTime() public view returns(uint){
+        return block.timestamp;
     }
     
     // allowing developers to submit their score in order to determine if they're the current leaders
-    function contractSubmission(uint score) public startContract returns(bool){
-        if(block.timestamp < endTime){    
-            if(score >= leaderScore){
-                return false;
-            }
-            
-            leaderScore = score;
-            leaderAddress = msg.sender;
-            
-            return true;
-        }
-        else{
-            return false;
+    function contractSubmission(uint score) public contractIsFunded notOwner beforeEndTime{
+        if(score < leaderScore){
+        leaderScore = score;
+        leaderAddress = msg.sender;
         }
     }
     
-    function beginContract(uint _endTime) public onlyOwner{
+    function beginContract(uint _endTime) public payable onlyOwner{
+        require(contractAmount == msg.value);
         endTime = _endTime;
-        contractStarted = true;
-    }
-    
-    function endContract() public onlyOwner{
-        contractStarted = false;
-    }
-    
-    function addMoreContractFunds() public payable {
-        require(msg.value >= minimumContractAmount);
-        contractAmount += msg.value;
+        contractFunded = true;
     }
     
     // only winner can withdraw funds
-    function winnerWithdrawal() public contractEnded{        
-        if(block.timestamp < endTime){ 
-            if(payable(msg.sender).send(contractAmount)){
-                contractAmount -= contractAmount;
-                contractStarted = false;
-                leaderAddress = address(0);
-                leaderScore = 100;
-                // THOUGHTS: WE CAN OPTIONALLY RESET THE OWNER TO A ZERO ADDRESS TO END THE CONTRACT
-            }
+    function winnerWithdrawal() public pastEndTime onlyLeader{        
+        if(payable(msg.sender).send(contractAmount)){
+            contractAmount -= contractAmount;
+            contractFunded = false;
+            leaderAddress = address(0);
+            leaderScore = 100;
+            // THOUGHTS: WE CAN OPTIONALLY RESET THE OWNER TO A ZERO ADDRESS TO END THE CONTRACT
         }
     }
     
+    function ownerWithdrawal() public onlyOwner ownerWithdrawalValidation{
+        if(payable(msg.sender).send(contractAmount)){
+            contractAmount -= contractAmount;
+            contractFunded = false;
+            leaderAddress = address(0);
+            leaderScore = 100;
+            // THOUGHTS: WE CAN OPTIONALLY RESET THE OWNER TO A ZERO ADDRESS TO END THE CONTRACT
+        }
+    }
 }  
+
  
