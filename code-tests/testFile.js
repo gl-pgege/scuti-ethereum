de8b9cfe058f92cab503544f4d5d1f118cd80e5f
paulyg321 <19629540+paulyg321@users.noreply.github.com>
2021-07-20 14:26:51 -0400

    maps and arrays now supported for test values

--- code-tests/testFile.js
+++ code-tests/testFile.js
@@ -4,26 +4,29 @@ const solc = require("solc");
 const Web3 = require("web3");
 const ganache = require("ganache-cli");
 const _ = require("lodash");
-var expect = require('chai').expect;
+const expect = require('chai').expect;
+const { reject } = require("lodash");
 
 const web3 = new Web3(ganache.provider());
 const GAS = 6700000;
 const GASPRICE = '97000000000';
-const VALUE = 1000;
 
 const transactionTypes = {
     deploy: "DEPLOY",
     functionCall: "FUNCTION_CALL",
 }
 
-function compileCode(contractName){
+function extractFileNameFromPath(path){
+    return path.replace(/^.*[\\\/]/, '');
+}
 
-    const contractFileName = `${contractName}.sol`;
+// TODO: Add ability to change compiler versions based on the contract specified compiler versions
+function compileContract(contractPath){
 
-    // needs to adjust to contract name
-    const inboxPath = path.resolve(__dirname, "contracts", contractFileName);
+    const contractFileName = extractFileNameFromPath(contractPath);
+    const contractName = contractFileName.replace(".sol", "");
         
-    const source = fs.readFileSync(inboxPath, "utf-8");
+    const source = fs.readFileSync(contractPath, "utf-8");
 
     let sources = {}
 
@@ -79,14 +82,20 @@ function generateTxnSendArgumentObj(account, explicitGas, payableAmount=0){
 }
 
 async function generateAccountsBasedOnRoles(rolesArray){
-    const userAccounts = await web3.eth.getAccounts();
-    let accountsObj = {}
-
-    rolesArray.forEach((role, index) => {
-        accountsObj[role] = userAccounts[index]
+    return new Promise(async (resolve, reject) => {
+        try {
+            const userAccounts = await web3.eth.getAccounts();
+            let accountsObj = {}
+        
+            rolesArray.forEach((role, index) => {
+                accountsObj[role] = userAccounts[index]
+            })
+        
+            resolve(accountsObj);
+        } catch (error){
+            reject(error)
+        }
     })
-
-    return accountsObj
 }
 
 async function sendAndCalculateGasUsedForTxn(account, transaction, txnType, explicitGas=false, payableAmount=0){
@@ -105,38 +114,25 @@ async function sendAndCalculateGasUsedForTxn(account, transaction, txnType, expl
             break;
     }
     
-    console.log(sendDetails);
-    
-    const initialAccountBalance = await web3.eth.getBalance(account);
-    const txnReturnValue = await transaction.send({
-        ...sendDetails
-    });
-    const finalAccountBalance = await web3.eth.getBalance(account);
-    const gasUsed = initialAccountBalance - finalAccountBalance;
+    return new Promise(async (resolve, reject) => {
+        
+        try {
+            const initialAccountBalance = await web3.eth.getBalance(account);
+            const txnReturnValue = await transaction.send({
+                ...sendDetails
+            });
+            const finalAccountBalance = await web3.eth.getBalance(account);
+            const gasUsed = initialAccountBalance - finalAccountBalance;
 
-    console.log("gasUsed", gasUsed);
+            resolve({
+                gasUsed,
+                txnReturnValue
+            });
+        } catch (error) {
+            reject(error);
+        }
 
-    return {
-        gasUsed,
-        txnReturnValue
-    };
-}
-
-const expectThrowsAsync = async (method) => {
-    let error = null
-    try {
-        await method()
-    }
-    catch (err) {
-        error = err
-    }
-
-    
-    try{
-        console.log("Pass - ", expect(error.message).to.not.be.empty)
-    } catch(error){
-        console.log("Fail - ", error);
-    }
+    });    
 }
 
 function isAddress(property){
@@ -160,7 +156,7 @@ function replaceAddressPlaceholdersWithAccounts(funcArguments, availableAccounts
             const parsedArgument = argument.replace('[', '').replace(']', '');
             if(parsedArgument in availableAccounts){
                 parsedFuncArgs.push(availableAccounts[parsedArgument]);
-            } else {
+            } else {contestTestJsonObject
                 // TODO: Create an error object with all error messages you'll need & keep it updated
                 throw new Error("Account does not exist within availableAccounts, please ensure specify an account that exists")
             }
@@ -173,107 +169,305 @@ function replaceAddressPlaceholdersWithAccounts(funcArguments, availableAccounts
 }
 
 
-async function deployContract(contractName, constructorSettings, accounts){
+async function deployContract(contractPath, constructorSettings, accounts){
     // Contract compilation needs to adjust to contract name
-    const {abi, evm} = compileCode(contractName);
+    const {abi, evm} = compileContract(contractPath);
+    console.log(abi);
     const {
         arguments: constructorArguments, 
         payableAmount: constructorPayableAmount,
         deploymentAccount
     } = constructorSettings;
 
-    const parsedConstructorArguments = replaceAddressPlaceholdersWithAccounts(constructorArguments, accounts)
-    console.log(parsedConstructorArguments);
+    const parsedConstructorArguments = replaceAddressPlaceholdersWithAccounts(constructorArguments, accounts);
 
-    let transaction = await new web3.eth.Contract(abi).deploy({
-        data: `0x${evm.bytecode.object}`,
-        // IF CONSTRUCTOR ACCEPTS ARGUMENTS WE PASS THEM WITH OUR SETTINGS
-        arguments: parsedConstructorArguments,
-    })
+    return new Promise( async (resolve, reject) => {
+        try {
+            let transaction = await new web3.eth.Contract(abi).deploy({
+                data: `0x${evm.bytecode.object}`,
+                arguments: parsedConstructorArguments,
+            })
+        
+            // deploy smart contract
+            let {gasUsed, txnReturnValue: contract} = await sendAndCalculateGasUsedForTxn(accounts[deploymentAccount], transaction, transactionTypes.deploy, true, constructorPayableAmount);
+    
+            resolve({
+                gasUsed,
+                contract
+            })
 
-    // deploy smart contract
-    let {gasUsed, txnReturnValue: contract} = await sendAndCalculateGasUsedForTxn(accounts[deploymentAccount], transaction, transactionTypes.deploy, true, constructorPayableAmount);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
 
+function generateTestResults(passedTests, failedTests, gasSpent){
     return {
-        gasUsed,
-        contract
+        pass: passedTests.length,
+        fail: failedTests.length,
+        failedTests,
+        gasUsed: gasSpent
     }
 }
 
-async function testContract(contractName, constructorSettings, testCases){
+async function expectThrowsAsync(method) {
 
-    const {accountsNeeded, deploymentAccount} = constructorSettings;
-    const userAccounts = await generateAccountsBasedOnRoles(accountsNeeded);
+    let error = null
+    try {
+        await method()
+    } catch (err) {
+        error = err
+    }
 
-    try{
+    expect(error).to.not.be.null        
+}
+
+function expectWrapper(expectedOutput, actualOutput){
+    try {
+        if(typeof expectedOutput === "object"){
+            expect(expectedOutput).to.deep.equal(actualOutput)
+        } else {
+            expect(expectedOutput).to.equal(actualOutput)
+        }
+    } catch(error) {
+        throw new Error(error)
+    }
+    
+}
+
+function isSolidityMap(value){
+    return Boolean(value.includes("[")) && Boolean(value.includes("]"))
+}
+
+function extractPropertyFromValueToCheck(value){
+    let property;
+    let mapName;
+
+    const matches = value.match(/\[(.*?)\]/);
+
+    if (matches) {
+        property = matches[1];
+    }
+
+    mapName = value.replace(property, "")
+                        .replace("[", "")
+                        .replace("]", "");
+
+    return {
+        property,
+        mapName
+    }
+}
+
+async function testSolidityFunction(contract, testCase){
+    
+    return new Promise(async (resolve, reject) => {
+        const {
+            functionName, 
+            expectedOutput, 
+            valueToCheck, 
+            negativeTest,
+        } = testCase;
         
-        let {gasUsed, contract: initialContract} = await deployContract(contractName, constructorSettings, userAccounts)
-        // gasSpent += gasUsed;        
-
-        for(let i = 0; i < testCases.length; i++){
-            let contract = initialContract;
-
-            const {
-                functionName, 
-                arguments, 
-                expectedOutput, 
-                valueToCheck, 
-                account,
-                negativeTest,
-                payableAmount,
-                resetContract
-            } = testCases[i];
-
-            const parsedArguments = replaceAddressPlaceholdersWithAccounts(arguments, userAccounts);
-
-            if(resetContract){
-                const {gasUsed: newContractGasUse, contract: newContract} = await deployContract(contractName, constructorSettings, userAccounts);
-                gasUsed = newContractGasUse,
-                contract = newContract;
+        let actualOutput;
+    
+        let arrayTestState;
+    
+        if(Array.isArray(expectedOutput)){
+            arrayTestState = {
+                passed: 0,
+                failed: 0,
+                failures: []
             }
-
-            transaction = contract.methods[functionName](...parsedArguments);
-
-            if(negativeTest){
-                await expectThrowsAsync(
-                    (async function(){
-                        return await transaction.send({
-                            from: userAccounts[account]
-                        });
-                    })
-                );
+    
+            for(let i = 0; i < expectedOutput.length; i++){
+                actualOutput = await contract.methods[valueToCheck](i).call()
+    
+                try{
+                    expectWrapper(expectedOutput[i], actualOutput); 
+                    arrayTestState.passed += 1;         
+                } catch(error){
+                    arrayTestState.failed += 1;
+                    arrayTestState.failures.push(
+                        {
+                            index: i,
+                            issue: error.message
+                        }
+                    )
+                }
+            }
+            
+            console.log(arrayTestState.failures);
+            if(arrayTestState.failed > 0){
+                reject({
+                    functionName,
+                    valueToCheck,
+                    negativeTest,
+                    issue: arrayTestState.failures
+                })
             } else {
-                
-                let { gasUsed } = await sendAndCalculateGasUsedForTxn(userAccounts[account], transaction, transactionTypes.functionCall, false ,payableAmount);
-                // gasSpent += gasUsed;
+                resolve({
+                    functionName,
+                    valueToCheck,
+                    negativeTest,
+                })
             }
+            
+        } else {
 
-            let actualOutput = await contract.methods[valueToCheck]().call()
-
+            if(isSolidityMap(valueToCheck)){
+                const {property, mapName} = extractPropertyFromValueToCheck(valueToCheck)
+                actualOutput = await contract.methods[mapName](property).call();
+            } else {
+                actualOutput = await contract.methods[valueToCheck]().call();
+            }
+    
+            // TODO: Make the following code snippets into a function to follow DRY principles
             if(typeof expectedOutput === "number"){
                 actualOutput = parseInt(actualOutput);
             }
-
+    
             try{
-                console.log("Pass - ", expect(expectedOutput).to.equal(actualOutput));
+                
+                if(typeof expectedOutput === "object"){
+                    expect(expectedOutput).to.deep.equal(actualOutput)
+                } else {
+                    expect(expectedOutput).to.equal(actualOutput)
+                }
+                
+                
+                resolve({
+                    functionName,
+                    valueToCheck,
+                    negativeTest,
+                })
             } catch(error){
-                console.log("Fail - ", error);
+    
+                reject({
+                    functionName,
+                    valueToCheck,
+                    negativeTest,
+                    issue: error.message
+                })
             }
         }
-
-        // console.log("gas spent", gasSpent);
-    } catch(error){
-        console.log(error);
-    }
+    })
 }
 
+async function testContract(contractPath, constructorSettings, testCases){
+
+    let passedTests = [];
+    let failedTests = [];
+    let gasSpent = 0;
+
+    const {accountsNeeded} = constructorSettings;
+    
+    const userAccounts = await generateAccountsBasedOnRoles(accountsNeeded);
+
+    return new Promise(async (resolve, reject) => {
+        try{
+        
+            let {gasUsed: deploymentCost, contract: initialContract} = await deployContract(contractPath, constructorSettings, userAccounts)
+            // GAS CALCULATION
+            gasSpent += deploymentCost;        
+    
+            for(let i = 0; i < testCases.length; i++){
+                let contract = initialContract;
+    
+                const {
+                    functionName, 
+                    arguments, 
+                    valueToCheck, 
+                    account,
+                    negativeTest,
+                    payableAmount,
+                    resetContract
+                } = testCases[i];
+    
+                const parsedArguments = replaceAddressPlaceholdersWithAccounts(arguments, userAccounts);
+    
+                if(resetContract){
+                    const {gasUsed: newContractDeploymentCost , contract: newContract} = await deployContract(contractPath, constructorSettings, userAccounts);
+                    // GAS CALCULATION
+                    gasSpent += newContractDeploymentCost;        
+    
+                    contract = newContract;
+                }
+    
+                transaction = contract.methods[functionName](...parsedArguments);
+    
+                if(negativeTest){
+                    
+                    try{
+                        await expectThrowsAsync(
+                            async function(){
+                                let {gasUsed: failingTransactionCost} = await sendAndCalculateGasUsedForTxn(
+                                    userAccounts[account], 
+                                    transaction, 
+                                    transactionTypes.functionCall, 
+                                    true, 
+                                    payableAmount
+                                )
+                                // GAS CALCULATION
+                                gasSpent += failingTransactionCost;        
+                            }
+                        );
+    
+                        passedTests.push({
+                            functionName,
+                            valueToCheck,
+                            negativeTest,
+                        })
+    
+                    } catch (error) {
+                        failedTests.push({
+                            functionName,
+                            valueToCheck,
+                            negativeTest,
+                            issue: "expected transaction to revert"
+                        })
+                    }
+                } else {
+                    
+                    let { gasUsed: transactionCost } = await sendAndCalculateGasUsedForTxn(userAccounts[account], transaction, transactionTypes.functionCall, true ,payableAmount);
+                    // GAS CALCULATION
+                    gasSpent += transactionCost;        
+                }
+    
+                try{
+                    passedTests.push(await testSolidityFunction(contract, testCases[i]))
+                } catch (error) {
+                    failedTests.push(error)
+                }        
+            }
+    
+            const testResults = generateTestResults(passedTests, failedTests, gasSpent);
+            
+            resolve(testResults)
+        } catch(error){
+            reject(error);
+        }
+    })
+}
 
 const mathematicTestJsonObj = [
     {
-        functionName: "add",
-        arguments: [1, 2, 3],
-        expectedOutput: 6,
-        valueToCheck: "answer",
+        functionName: "addBook",
+        arguments: [{
+            title: "test",
+            author: "paul",
+            book_id: 12
+        }],
+        expectedOutput: {
+            "0": "test",
+            "1": "paul",
+            "2": "12",
+            title: "test",
+            author: "paul",
+            book_id: "12"
+        },
+        valueToCheck: "book",
         account: "user1",
         negativeTest: false,
         payableAmount: 0,
@@ -283,42 +477,42 @@ const mathematicTestJsonObj = [
         functionName: "subtract",
         arguments: [10, 2],
         expectedOutput: 8,
-        valueToCheck: "answer",
+        valueToCheck: "answers[subtract]",
         account: "user1",
         negativeTest: false,
         payableAmount: 0,
         resetContract: false
     },
-    {
-        functionName: "multiply",
-        arguments: [1, 2, 3, 4],
-        expectedOutput: 24,
-        valueToCheck: "answer",
-        account: "user1",
-        negativeTest: false,
-        payableAmount: 0,
-        resetContract: false 
-    },
-    {
-        functionName: "divide",
-        arguments: [4, 0],
-        expectedOutput: 0,
-        valueToCheck: "answer",
-        account: "user1",
-        negativeTest: true,
-        payableAmount: 0,
-        resetContract: true
-    },
-    {
-        functionName: "divide",
-        arguments: [4, 2],
-        expectedOutput: 2,
-        valueToCheck: "answer",
-        account: "user1",
-        negativeTest: false,
-        payableAmount: 0,
-        resetContract: false
-    },
+    // {
+    //     functionName: "multiply",
+    //     arguments: [1, 2, 3, 4],
+    //     expectedOutput: 24,
+    //     valueToCheck: "answer",
+    //     account: "user1",
+    //     negativeTest: false,
+    //     payableAmount: 0,
+    //     resetContract: false 
+    // },
+    // {
+    //     functionName: "divide",
+    //     arguments: [4, 0],
+    //     expectedOutput: 0,
+    //     valueToCheck: "answer",
+    //     account: "user1",
+    //     negativeTest: true,
+    //     payableAmount: 0,
+    //     resetContract: true
+    // },
+    // {
+    //     functionName: "divide",
+    //     arguments: [4, 2],
+    //     expectedOutput: 2,
+    //     valueToCheck: "answer",
+    //     account: "user1",
+    //     negativeTest: false,
+    //     payableAmount: 0,
+    //     resetContract: false
+    // },
 ]
 
 // TODO: Find a standard/way to specify that an address should be used as an argument
@@ -411,5 +605,12 @@ const contestConstructorDetails = {
     deploymentAccount: 'contract_owner'    
 }
 
-testContract("Contest", contestConstructorDetails, contestTestJsonObject);
-// testContract("Mathematics", mathematicsConstructorDetails, mathematicTestJsonObj);
+// testContract("./contracts/Contest.sol", contestConstructorDetails, contestTestJsonObject);
+
+async function main(){
+    const testResults = await testContract("./contracts/Mathematics.sol", mathematicsConstructorDetails, mathematicTestJsonObj);
+
+    console.log(testResults);
+}
+
+main();
