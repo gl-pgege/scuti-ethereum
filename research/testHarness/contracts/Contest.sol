bcb3e48d9c547174a2db85cbc92fab360ea5bb21
paulyg321 <19629540+paulyg321@users.noreply.github.com>
2021-07-27 14:23:01 -0400

    code refactoring

---
+++ research/testHarness/contracts/Contest.sol
@@ -0,0 +1,102 @@
+pragma solidity 0.6.12;
+
+contract Contest {
+    address public owner;
+
+    uint public endTime;
+    
+    uint public leaderScore;
+    address public leaderAddress;
+    
+    // for contract to be started you need to have contract amount > 0
+    bool public contractFunded;
+    uint public contractAmount;
+
+    // Modifiers
+    modifier onlyOwner(){
+        require(owner == msg.sender);
+        _;
+    }
+
+    modifier contractIsFunded(){
+        require(contractFunded);
+        _;
+    }
+    
+    modifier pastEndTime(){
+        require(block.timestamp > endTime);
+        _;
+    }
+    
+    modifier beforeEndTime(){
+        require(block.timestamp < endTime);
+        _;
+    }
+    
+    modifier onlyLeader(){
+        require(leaderAddress == msg.sender);
+        _;
+    }
+    
+    modifier notOwner(){
+        require(owner != msg.sender);
+        _;
+    }
+    
+    modifier ownerWithdrawalValidation(){
+        require(leaderAddress == address(0) || (leaderAddress == address(0) && block.timestamp > endTime));
+        _;
+    }
+
+    // constructor that ensures the contract owner funds the contract on deployment
+    constructor(address _owner, uint _contractAmount) public{
+        owner = _owner;
+        contractAmount = _contractAmount;
+        leaderScore = 100;
+    }
+    
+    function getOwner() public view returns(address) {
+        return msg.sender;
+    }
+    
+    function currentTime() public view returns(uint){
+        return block.timestamp;
+    }
+    
+    // allowing developers to submit their score in order to determine if they're the current leaders
+    function contractSubmission(uint score) public contractIsFunded notOwner beforeEndTime{
+        if(score < leaderScore){
+        leaderScore = score;
+        leaderAddress = msg.sender;
+        }
+    }
+    
+    function beginContract(uint _endTime) public payable onlyOwner{
+        require(contractAmount == msg.value);
+        endTime = _endTime;
+        contractFunded = true;
+    }
+    
+    // only winner can withdraw funds
+    function winnerWithdrawal() public pastEndTime onlyLeader{        
+        if(payable(msg.sender).send(contractAmount)){
+            contractAmount -= contractAmount;
+            contractFunded = false;
+            leaderAddress = address(0);
+            leaderScore = 100;
+            // THOUGHTS: WE CAN OPTIONALLY RESET THE OWNER TO A ZERO ADDRESS TO END THE CONTRACT
+        }
+    }
+    
+    function ownerWithdrawal() public onlyOwner ownerWithdrawalValidation{
+        if(payable(msg.sender).send(contractAmount)){
+            contractAmount -= contractAmount;
+            contractFunded = false;
+            leaderAddress = address(0);
+            leaderScore = 100;
+            // THOUGHTS: WE CAN OPTIONALLY RESET THE OWNER TO A ZERO ADDRESS TO END THE CONTRACT
+        }
+    }
+}  
+
+ 
---
+++ research/testHarness/contracts/Mathematics.sol
@@ -0,0 +1,39 @@
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+contract Mathematics {
+
+    struct Book {
+        string title;
+        string author;
+        uint book_id;
+    }
+
+    mapping(string => uint) public answers;
+
+    uint public answer;
+    uint public random;
+
+    Book[] public book;
+
+    function add(uint value1, uint value2, uint value3) public {
+        answers["add"] = value1 + value2 + value3;
+    }
+    
+    function subtract(uint value1, uint value2) public {
+        answers["subtract"] = value1 - value2;
+    }
+    
+    function multiply(uint value1, uint value2, uint value3, uint value4) public {
+        answers["multiply"] = value1 * value2 * value3 * value4;
+    }
+
+    function addBook(Book memory _book) public {
+        book.push(_book);
+    }
+
+    function divide(uint value1, uint value2) public {
+        require(value2 != 0);
+        answers["divide"] = value1/value2;
+    }
+}
